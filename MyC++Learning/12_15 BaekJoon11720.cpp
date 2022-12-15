471164aa77e83586af55262828a698f4b8fdc96a
rlagmltn <gmltn2015@naver.com>
2022-12-15 15:00:43 +0900

    06 문자열 풀이중

---
+++ MyC++Learning/12_15 BaekJoon11720.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//
+//int main()
+//{	char ch;
+//	char s[101];
+//	string s;
+//	int m, n = 0;
+//	cin >> m;
+//
+//	//for (int i = 0; i < m; i++) // char
+//	//{
+//	//	cin >> ch;
+//	//	n += ch - '0';
+//	//}
+//
+//	//for (int i = 0; i < m; i++) // like string
+//	//{
+//	//	cin >> s[i];
+//	//  cin >> s;
+//	//}
+//	//for (int i = 0; i < m; i++)
+//	//{
+//	//	n += s[i] - '0';
+//	//}
+//
+//	cout << n;
+//}
---
+++ MyC++Learning/12_15 Baekjoon10809.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{
+//	int spel[26] = { 0 };
+//	string s;
+//	cin >> s;
+//	for (int i = 0; i < 27; i++)
+//		spel[i] = -1;
+//	for (int i = 0; i < s.size(); i++)
+//	{
+//		if (spel[s[i] - 'a'] == -1)
+//			spel[s[i] - 'a'] = i;
+//	}
+//	for (int i : spel)
+//	{
+//		cout << i << " ";
+//	}
+//}
