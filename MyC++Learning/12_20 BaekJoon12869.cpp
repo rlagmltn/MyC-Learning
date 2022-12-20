2b6a829fcbdaabab11a6db0ed92de747488debfa
rlagmltn <gmltn2015@naver.com>
2022-12-20 20:21:45 +0900

    주석

--- MyC++Learning/12_20 BaekJoon12869.cpp
+++ MyC++Learning/12_20 BaekJoon12869.cpp
@@ -1,54 +1,54 @@
-#include <iostream>
-#include<string.h>
-using namespace std;
-
-int n;
-int scv[3];
-int memo[61][61][61];
-int damage[6][3]{
-	9,3,1,
-	9, 1,3,
-	3, 1, 9,
-	3, 9, 1,
-	1, 3,9,
-	1, 9, 3
-};
-
-int solve(int a, int b, int c)
-{
-	if (a == 0 && b == 0 && c == 0) {
-		return 0;
-	}
-	if (a < 0) {
-		return solve(0, b, c);
-	}
-	if (b < 0) {
-		return solve(a, 0, c);
-	}
-	if (c < 0) {
-		return solve(a, b, 0);
-	}
-	if (memo[a][b][c] != -1) {
-		return memo[a][b][c];
-	}
-	memo[a][b][c] = 999;
-
-	for (int i = 0; i < 6; i++)
-	{
-		memo[a][b][c] = min(memo[a][b][c], solve(a - damage[i][0], b - damage[i][1], c - damage[i][2]) + 1);
-	}
-
-	return memo[a][b][c];
-}
-
-int main() {
-	memset(memo, -1, sizeof(memo));
-
-	cin >> n;
-
-	for (int i = 0; i < n; i++) {
-		cin >> scv[i];
-	}
-
-	cout << solve(scv[0], scv[1], scv[2]);
-}
+//#include <iostream>
+//#include<string.h>
+//using namespace std;
+//
+//int n;
+//int scv[3];
+//int memo[61][61][61];
+//int damage[6][3]{
+//	9,3,1,
+//	9, 1,3,
+//	3, 1, 9,
+//	3, 9, 1,
+//	1, 3,9,
+//	1, 9, 3
+//};
+//
+//int solve(int a, int b, int c)
+//{
+//	if (a == 0 && b == 0 && c == 0) {
+//		return 0;
+//	}
+//	if (a < 0) {
+//		return solve(0, b, c);
+//	}
+//	if (b < 0) {
+//		return solve(a, 0, c);
+//	}
+//	if (c < 0) {
+//		return solve(a, b, 0);
+//	}
+//	if (memo[a][b][c] != -1) {
+//		return memo[a][b][c];
+//	}
+//	memo[a][b][c] = 999;
+//
+//	for (int i = 0; i < 6; i++)
+//	{
+//		memo[a][b][c] = min(memo[a][b][c], solve(a - damage[i][0], b - damage[i][1], c - damage[i][2]) + 1);
+//	}
+//
+//	return memo[a][b][c];
+//}
+//
+//int main() {
+//	memset(memo, -1, sizeof(memo));
+//
+//	cin >> n;
+//
+//	for (int i = 0; i < n; i++) {
+//		cin >> scv[i];
+//	}
+//
+//	cout << solve(scv[0], scv[1], scv[2]);
+//}
--- MyC++Learning/12_20 BaekJoon1890.cpp
+++ MyC++Learning/12_20 BaekJoon1890.cpp
@@ -1,39 +1,39 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-int n;
-long long memo[100][100];
-long long map[100][100];
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> map[i][j];
-        }
-    }
-
-    memo[0][0] = 1;
-    for (int y = 0; y < n; y++) {
-        for (int x = 0; x < n; x++) {
-            if (map[y][x] == 0) continue;
-            if (memo[y][x] != 0) {
-                int nextY = y + map[y][x];
-                int nextX = x + map[y][x];
-
-                if (nextY < n) {
-                    memo[nextY][x] += memo[y][x];
-                }
-                if (nextX < n) {
-                    memo[y][nextX] += memo[y][x];
-                }
-            }
-        }
-    }
-
-    cout << memo[n - 1][n - 1];
-}
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int n;
+//long long memo[100][100];
+//long long map[100][100];
+//
+//int main() {
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(NULL); cout.tie(NULL);
+//
+//    cin >> n;
+//    for (int i = 0; i < n; i++) {
+//        for (int j = 0; j < n; j++) {
+//            cin >> map[i][j];
+//        }
+//    }
+//
+//    memo[0][0] = 1;
+//    for (int y = 0; y < n; y++) {
+//        for (int x = 0; x < n; x++) {
+//            if (map[y][x] == 0) continue;
+//            if (memo[y][x] != 0) {
+//                int nextY = y + map[y][x];
+//                int nextX = x + map[y][x];
+//
+//                if (nextY < n) {
+//                    memo[nextY][x] += memo[y][x];
+//                }
+//                if (nextX < n) {
+//                    memo[y][nextX] += memo[y][x];
+//                }
+//            }
+//        }
+//    }
+//
+//    cout << memo[n - 1][n - 1];
+//}
