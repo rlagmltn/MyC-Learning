9faecfd57ad99c26e4b6858fb7af67cf6ef4336d
rlagmltn <gmltn2015@naver.com>
2022-12-29 20:22:42 +0900

    오늘 끝

--- MyC++Learning/12_28 BaekJoon2292.cpp
+++ MyC++Learning/12_28 BaekJoon2292.cpp
@@ -1,21 +1,29 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	int n, sum = 5, cnt = 2	;
-	cin >> n;
-	if (n == 1)
-	{	
-		cout << n;
-		return 0;
-	}
-	for (int i = 2; i < n; i += sum + 1, sum += 6, cnt++)
-	{
-		if (i <= n && i + sum > n)
-		{
-			cout << cnt;
-			return 0;
-		}
-	}
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n, sum = 2, cnt = 0;
+//	cin >> n;
+//	if (n == 1)
+//	{	
+//		cout << n;
+//		return 0;
+//	}
+//	//// 일반
+//	//while (n >= sum)
+//	//{
+//	//	sum += 6 * (cnt++);
+//	//}
+//	
+//	//// 계차수항
+//	//cnt = 1;
+//	//sum = 0;
+//	//while (n >= sum)
+//	//{
+//	//	cnt++;
+//	//	sum = 2 + 3 * cnt * (cnt - 1);
+//	//}
+//
+//	cout << cnt;
+//}
