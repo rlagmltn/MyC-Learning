9575e0504ed865d44ba90ec4267f5bd9a53664e5
rlagmltn <gmltn2015@naver.com>
2023-03-02 12:58:28 +0900

    1단계 문제 복구 및 추가된 문제 풀이 완료

---
+++ MyC++Learning/11_25 BaekJoon2588.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    int a, b;
+//    cin >> a;
+//    cin >> b;
+// 
+//    cout << a * (b % 10) << "\n";
+//    cout << a * (b % 100 / 10) << "\n";
+//    cout << a * (b / 100) << "\n";
+//    cout << a * b << "\n";
+//}
