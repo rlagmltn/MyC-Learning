9f779c867d309ce31b2b988d0eb2ae117b90558d
rlagmltn <gmltn2015@naver.com>
2023-03-06 09:33:36 +0900

    문제 4 끝

--- MyC++Learning/23_03_02 BaekJoon10811.cpp
+++ MyC++Learning/23_03_02 BaekJoon10811.cpp
@@ -1,33 +1,34 @@
-#include <iostream>
-using namespace std;
-int a[101];
-
-void Swap(int x, int y)
-{
-	int r = (x + y) / 2, t;
-	for (int i = 0; i < r; i++)
-	{
-		t = a[x + i];
-		a[x + i] = a[y - i];
-		a[y - i] = t;
-	}
-}
-void Init(int n)
-{
-	for (int i = 1; i <= n; i++)
-		a[i] = i;
-}
-int main()
-{
-	int m, n;
-	cin >> n >> m;
-	Init(n);
-	for (int i = 0; i < m; i++)
-	{
-		int x, y;
-		cin >> x >> y;
-		Swap(x, y);
-	}
-	for (int i = 1; i <= n; i++)
-		cout << a[i] << " ";
-}
+//#include <iostream>
+//using namespace std;
+//int a[101];
+//
+//void Swap(int x, int y)
+//{
+//	while (x < y)
+//	{
+//		int t = a[x];
+//		a[x] = a[y];
+//		a[y] = t;
+//
+//		x++, y--;
+//	}
+//}
+//void Init(int n)
+//{
+//	for (int i = 1; i <= n; i++)
+//		a[i] = i;
+//}
+//int main()
+//{
+//	int m, n;
+//	cin >> n >> m;
+//	Init(n);
+//	for (int i = 0; i < m; i++)
+//	{
+//		int x, y;
+//		cin >> x >> y;
+//		Swap(x, y);
+//	}
+//	for (int i = 1; i <= n; i++)
+//		cout << a[i] << " ";
+//}
